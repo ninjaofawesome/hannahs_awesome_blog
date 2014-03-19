91eb24cb84915cd57330ffee24a574e1afde2736
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-19 10:11:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HannahsAwesomeBlog::Application.config.secret_token = 'c0a6bf0dd6a9c5e2125cd212d08fef19c900245da92180ad599ab34875b21376419cb7a94fe5a75f5c6a63011bded6d28f2293e1b7441b8b49624df871996bc9'
