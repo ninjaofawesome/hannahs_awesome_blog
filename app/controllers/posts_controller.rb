3104e19c9287e072e21cf15250fe43a7959d8d40
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-24 18:06:01 -0400

    added exception for index for devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_filter :authenticate_user!
+	before_filter :authenticate_user!, :except => [:index]
 
 	def index 
 		@posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ HannahsAwesomeBlog::Application.routes.draw do
   resources :posts
 
   root :to => "posts#index"
+
+  
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
